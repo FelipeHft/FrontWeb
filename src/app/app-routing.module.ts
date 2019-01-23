d8867aa56d6e1f88bc5dadb3cd54c55a24e7fd22
pipeworkout <felipe.hernandezc@utem.cl>
2019-01-22 21:15:22 -0300

    modificacion rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeEstudianteComponent } from './pages/home-estudiante/home-estudiante.component';
+import { RamosEstudianteComponent } from './pages/ramos-estudiante/ramos-estudiante.component';
+import { HomeProfesorComponent } from './pages/home-profesor/home-profesor.component';
+import { RamosProfesorComponent } from './pages/ramos-profesor/ramos-profesor.component';
+import { RestablecerPwComponent } from './pages/restablecer-pw/restablecer-pw.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'home-estudiante', component: HomeEstudianteComponent },
+  { path: 'ramos-estudiante', component: RamosEstudianteComponent },
+  { path: 'home-profesor', component: HomeProfesorComponent },
+  { path: 'ramos-profesor', component: RamosProfesorComponent},
+  { path: 'restablecer', component: RestablecerPwComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
